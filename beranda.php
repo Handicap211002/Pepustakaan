7f5485c71c8c0e36006d3a566c8e0563e593157b
Handicap <deanhandicap@gmail.com>
2025-05-17 14:09:27 +0700

    update footer

--- beranda.php
+++ beranda.php
@@ -8,7 +8,6 @@
   <link rel="stylesheet" href="style/vars.css">
   <link rel="stylesheet" href="style/berandastyle.css">
 
-
   <style>
     a,
     button,
@@ -43,169 +42,139 @@
 
 <body>
   <div class="beranda">
-  <div class="rectangle-1" contenteditable="true" id="searchBox"></div>
-  <img class="search" src="svg/search0.svg" id="searchIcon" />
-  <div class="telusuri" id="placeholderText">Telusuri...</div>
+    <div class="rectangle-1" contenteditable="true" id="searchBox"></div>
+    <img class="search" src="svg/search0.svg" id="searchIcon" />
+    <div class="telusuri" id="placeholderText">Telusuri...</div>
     <div class="rekomendasi-bacaan">Rekomendasi Bacaan</div>
     <div class="rectangle-12" onclick="window.location.href='login.php'"></div>
     <div class="masuk" onclick="window.location.href='login.php'">Masuk</div>
     <div class="slider-container">
-  <div class="slider-track" id="sliderTrack">
-    <img src="img/pngtree-library-at-noon-15184440.png" alt="Slide 1" />
-    <img src="img/beranda-perpus-20.png" alt="Slide 2" />
-    <img src="img/perpus-beranda-10.png" alt="Slide 3" />
-  </div>
-</div>
-<div class="book-grid">
-  <a href="detail/timun-jelita.html" class="book-card">
-    <img src="img/image0.png" alt="Timun Jelita">
-    <p class="book-title">Timun Jelita</p>
-  </a>
-  <a href="detail/3726-mdpl.html" class="book-card">
-    <img src="img/image1.png" alt="3726 Mdpl">
-    <p class="book-title">3726 Mdpl</p>
-  </a>
-  <a href="detail/ayah-arahnya.html" class="book-card">
-    <img src="img/image2.png" alt="Ayah ini arahnya ke mana ya?">
-    <p class="book-title">Ayah ini arahnya ke mana ya?</p>
-  </a>
-  <a href="detail/berandal-bandung.html" class="book-card">
-    <img src="img/image3.png" alt="Berandal Bandung">
-    <p class="book-title">Berandal Bandung</p>
-  </a>
-  <a href="detail/web-programming.html" class="book-card">
-    <img src="img/buku-50.png" alt="Buku Mahir Web Programming">
-    <p class="book-title">Buku Mahir Web Programming</p>
-  </a>
-  <a href="detail/step-by-step.html" class="book-card">
-    <img src="img/buku-60.png" alt="Step by Step">
-    <p class="book-title">Step by Step</p>
-  </a>
-  <a href="detail/hujan-juni.html" class="book-card">
-    <img src="img/buku-70.png" alt="Hujan Bulan Juni">
-    <p class="book-title">Hujan Bulan Juni</p>
-  </a>
-  <a href="detail/bisnis-online.html" class="book-card">
-    <img src="img/buku-40.png" alt="Bisnis Online">
-    <p class="book-title">Bisnis Online</p>
-  </a>
+      <div class="slider-track" id="sliderTrack">
+        <img src="img/pngtree-library-at-noon-15184440.png" alt="Slide 1" />
+        <img src="img/beranda-perpus-20.png" alt="Slide 2" />
+        <img src="img/perpus-beranda-10.png" alt="Slide 3" />
+      </div>
+    </div>
 
-</div>
+    <div class="fitur-yang-kami-tawarkan">FITUR YANG KAMI TAWARKAN</div>
     <div class="rectangle-13" onclick="window.location.href='koleksibacaan.php'"></div>
     <div class="rectangle-14" onclick="window.location.href='beritaterkini.PHP'"></div>
     <div class="rectangle-15" onclick="window.location.href='manajemenbuku.php'"></div>
-    <img class="icon" src="svg/icon0.svg" />
-    <div class="fitur-yang-kami-tawarkan">FITUR YANG KAMI TAWARKAN</div>
     <div class="koleksi-bacaan" onclick="window.location.href='koleksibacaan.php'">koleksi bacaan</div>
     <div class="manajemen-buku" onclick="window.location.href='manajemenbuku.php'">Manajemen Buku</div>
     <div class="berita-terkini" onclick="window.location.href='beritaterkini.PHP'">Berita Terkini</div>
     <div class="koleksi-bacaan" onclick="window.location.href='koleksibacaan.php'">koleksi bacaan</div>
-    <div class="berita-terkini2">Berita Terkini</div>
-    <div class="footer">
-      <div class="footer2">
-        <img class="rectangle-27" src="svg/rectangle-270.svg" />
-        <div class="menu">
-          <div class="beranda2">Beranda</div>
-          <div class="koleksi-bacaan2">koleksi Bacaan</div>
-          <div class="berita-terkini3">berita Terkini</div>
-          <div class="manajemen-buku2">Manajemen Buku</div>
-          <div class="akun">Akun</div>
-        </div>
-        <div class="frame-89">
-          <div class="copyright-perpustakaan-online-all-rights-reserved-2025">
-            <span>
-              <span
-                class="copyright-perpustakaan-online-all-rights-reserved-2025-span">
-                Copyright
-              </span>
-              <span
-                class="copyright-perpustakaan-online-all-rights-reserved-2025-span2">
-                PERPUSTAKAAN ONLINE
-              </span>
-              <span
-                class="copyright-perpustakaan-online-all-rights-reserved-2025-span">
-                All Rights Reserved, 2025
-              </span>
-            </span>
-          </div>
-        </div>
-        <div class="line-3"></div>
-        <div class="perpustakaan-online">PERPUSTAKAAN ONLINE</div>
-        <div class="usefull-links">Usefull Links</div>
-        <div class="hubungi-kami">Hubungi Kami</div>
-        <div
-          class="perpustakaan-merupakan-tempat-dimana-banyak-menyimpan-berbagai-macam-buku-ilmu-pengetahuan-yang-banyak-bermanfaat-serta-tempat-banyak-melakukan-aktivitas-membaca">
-          Perpustakaan merupakan tempat dimana banyak menyimpan berbagai macam
-          buku ilmu pengetahuan yang banyak bermanfaat, serta tempat banyak
-          melakukan aktivitas membaca
-        </div>
-        <div class="perpustakaan-online-tanjungpinang-indonesia">
-          PERPUSTAKAAN ONLINE
-          <br />
-          Tanjungpinang
-          <br />
-          Indonesia
-        </div>
-        <div class="phone-081977549410-email-xxxxxxx-gmail-com">
-          Phone : 081977549410
-          <br />
-          Email : xxxxxxx@gmail.com
-        </div>
-        <img
-          class="whats-app-image-2025-04-16-at-08-57-48"
-          src="img/whats-app-image-2025-04-16-at-08-57-480.png" />
-      </div>
-      <div class="media-sosial">
-        <div class="frame-286">
-          <img class="icon-twitter" src="svg/icon-twitter0.svg" />
-        </div>
-        <div class="frame-286">
-          <img class="icon-facebook" src="svg/icon-facebook0.svg" />
-        </div>
-        <div class="frame-286">
-          <img class="icon-instagram" src="svg/icon-instagram0.svg" />
-        </div>
-        <div class="frame-286">
-          <img class="icon-youtube" src="svg/icon-youtube0.svg" />
-        </div>
-      </div>
-    </div>
-    <div class="berita-section">
-  <div class="berita-card">
-    <div class="rectangle"></div>
-    <img class="berita-img" src="img/image-10.png" alt="Gambar Berita 1" />
-    <h2 class="berita-judul">Jaksa Agung, Ahok, dan Korupsi Pertamina</h2>
-    <p class="berita-deskripsi">
-      dugaan korupsi tata kelola minyak mentah di anak perusahaan PT Pertamina, PT Pertamina Subholding dan Kontraktor Kontrak Kerja Sama (KKKS) tahun 2018-2023.
-    </p>
-  </div>
-
-  <div class="berita-card">
-    <div class="rectangle"></div>
-    <img class="berita-img" src="img/_1307332-7200.png" alt="Gambar Berita 2" />
-    <h2 class="berita-judul">Kuasa Hukum Pastikan Ijazah Jokowi Tak Pernah Hilang</h2>
-    <p class="berita-deskripsi">
-      Kuasa hukum Presiden Jokowi, Yusril Ihza Mahendra, menegaskan bahwa ijazah Jokowi asli dan tidak pernah hilang. Ia membantah tudingan ijazah palsu dan menyebutnya sebagai hoaks.
-    </p>
-  </div>
-</div>
-
     <img class="video" src="svg/video0.svg" />
     <img class="book-open" src="svg/book-open0.svg" />
     <img class="icon" src="svg/icon0.svg" />
+
+    <div class="book-grid">
+      <a href="detail/timun-jelita.html" class="book-card">
+        <img src="img/image0.png" alt="Timun Jelita">
+        <p class="book-title">Timun Jelita</p>
+      </a>
+      <a href="detail/3726-mdpl.html" class="book-card">
+        <img src="img/image1.png" alt="3726 Mdpl">
+        <p class="book-title">3726 Mdpl</p>
+      </a>
+      <a href="detail/ayah-arahnya.html" class="book-card">
+        <img src="img/image2.png" alt="Ayah ini arahnya ke mana ya?">
+        <p class="book-title">Ayah ini arahnya ke mana ya?</p>
+      </a>
+      <a href="detail/berandal-bandung.html" class="book-card">
+        <img src="img/image3.png" alt="Berandal Bandung">
+        <p class="book-title">Berandal Bandung</p>
+      </a>
+      <a href="detail/web-programming.html" class="book-card">
+        <img src="img/buku-50.png" alt="Buku Mahir Web Programming">
+        <p class="book-title">Buku Mahir Web Programming</p>
+      </a>
+      <a href="detail/step-by-step.html" class="book-card">
+        <img src="img/buku-60.png" alt="Step by Step">
+        <p class="book-title">Step by Step</p>
+      </a>
+      <a href="detail/hujan-juni.html" class="book-card">
+        <img src="img/buku-70.png" alt="Hujan Bulan Juni">
+        <p class="book-title">Hujan Bulan Juni</p>
+      </a>
+      <a href="detail/bisnis-online.html" class="book-card">
+        <img src="img/buku-40.png" alt="Bisnis Online">
+        <p class="book-title">Bisnis Online</p>
+      </a>
+    </div>
+
+    <div class="berita-terkini2">Berita Terkini</div>
+    <div class="berita-section">
+      <div class="berita-card">
+        <div class="rectangle"></div>
+        <img class="berita-img" src="img/image-10.png" alt="Gambar Berita 1" />
+        <h2 class="berita-judul">Jaksa Agung, Ahok, dan Korupsi Pertamina</h2>
+        <p class="berita-deskripsi">
+          dugaan korupsi tata kelola minyak mentah di anak perusahaan PT Pertamina, PT Pertamina Subholding dan Kontraktor Kontrak Kerja Sama (KKKS) tahun 2018-2023.
+        </p>
+      </div>
+      <div class="berita-card">
+        <div class="rectangle"></div>
+        <img class="berita-img" src="img/_1307332-7200.png" alt="Gambar Berita 2" />
+        <h2 class="berita-judul">Kuasa Hukum Pastikan Ijazah Jokowi Tak Pernah Hilang</h2>
+        <p class="berita-deskripsi">
+          Kuasa hukum Presiden Jokowi, Yusril Ihza Mahendra, menegaskan bahwa ijazah Jokowi asli dan tidak pernah hilang. Ia membantah tudingan ijazah palsu dan menyebutnya sebagai hoaks.
+        </p>
+      </div>
+    </div>
   </div>
+
+  <footer class="footer">
+    <div class="footer-top">
+      <img src="img/logo.png" alt="Logo" class="logo-footer" />
+      <div class="line"></div>
+      <div class="footer-section">
+        <h3>PERPUSTAKAAN ONLINE</h3>
+        <p>
+          Perpustakaan Merupakan Tempat Dimana Banyak Menyimpan Berbagai Macam Buku Ilmu
+          Pengetahuan Yang Banyak Bermanfaat, Serta Tempat Banyak Melakukan Aktivitas Membaca
+        </p>
+      </div>
+      <div class="footer-section usefull-links">
+        <h3>Usefull Links</h3>
+        <ul>
+          <li>Beranda</li>
+          <li>Koleksi Bacaan</li>
+          <li>Berita Terkini</li>
+          <li>Manajemen Buku</li>
+          <li>Akun</li>
+        </ul>
+      </div>
+      <div class="footer-section hubungi-kami">
+        <h3>Hubungi Kami</h3>
+        <p>PERPUSTAKAAN ONLINE<br>Tanjungpinang<br>Indonesia</p>
+        <p>Phone : 081977549410</p>
+        <p>Email : xxxxxxx@gmail.com</p>
+        <div class="social-icons">
+          <img src="svg/icon-twitter0.svg" alt="Twitter">
+          <img src="svg/icon-facebook0.svg" alt="Facebook">
+          <img src="svg/icon-instagram0.svg" alt="Instagram">
+          <img src="svg/icon-youtube0.svg" alt="YouTube">
+        </div>
+      </div>
+    </div>
+    <div class="footer-bottom">
+      <p>Copyright <span>PERPUSTAKAAN ONLINE</span> All Rights Reserved, 2025</p>
+    </div>
+  </footer>
+
   <script>
     const searchBox = document.getElementById("searchBox");
     const placeholderText = document.getElementById("placeholderText");
     const searchIcon = document.getElementById("searchIcon");
 
-    searchBox.addEventListener("input", function () {
+    searchBox.addEventListener("input", function() {
       const hasText = searchBox.innerText.trim().length > 0;
       placeholderText.classList.toggle("hidden", hasText);
       searchIcon.classList.toggle("hidden", hasText);
     });
 
-    searchBox.addEventListener("keydown", function (event) {
+    searchBox.addEventListener("keydown", function(event) {
       if (event.key === "Enter") {
         event.preventDefault();
         const keyword = searchBox.innerText.trim();
@@ -216,33 +185,31 @@
       }
     });
   </script>
+
   <script>
-  const track = document.getElementById("sliderTrack");
-  const totalSlides = 3;
-  const slideWidth = 1340 + 30; // gambar + gap
-  let currentIndex = 0;
+    const track = document.getElementById("sliderTrack");
+    const totalSlides = 3;
+    const slideWidth = 1340 + 30; // gambar + gap
+    let currentIndex = 0;
 
-  setInterval(() => {
-    currentIndex++;
-
-    // Kalau sudah di slide terakhir, langsung reset ke awal
-    if (currentIndex >= totalSlides) {
-      track.style.transition = "none"; // matikan animasi sementara
-      track.style.transform = "translateX(0px)";
-      currentIndex = 0;
-
-      // pakai timeout kecil agar animasi slide aktif lagi
-      setTimeout(() => {
-        track.style.transition = "transform 2s ease-in-out";
-        track.style.transform = `translateX(-${slideWidth}px)`;
-        currentIndex = 1;
-      }, 50);
-    } else {
-      track.style.transform = `translateX(-${currentIndex * slideWidth}px)`;
-    }
-  }, 4000); // waktu antar slide (4 detik)
-</script>
-
+    setInterval(() => {
+      currentIndex++;
+      // Kalau sudah di slide terakhir, langsung reset ke awal
+      if (currentIndex >= totalSlides) {
+        track.style.transition = "none"; // matikan animasi sementara
+        track.style.transform = "translateX(0px)";
+        currentIndex = 0;
+        // pakai timeout kecil agar animasi slide aktif lagi
+        setTimeout(() => {
+          track.style.transition = "transform 2s ease-in-out";
+          track.style.transform = `translateX(-${slideWidth}px)`;
+          currentIndex = 1;
+        }, 50);
+      } else {
+        track.style.transform = `translateX(-${currentIndex * slideWidth}px)`;
+      }
+    }, 4000); // waktu antar slide (4 detik)
+  </script>
 </body>
 
 </html>
