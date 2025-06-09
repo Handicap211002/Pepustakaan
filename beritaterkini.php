179391ca6991a91e632a7407a65d1575754676a9
Rachelhandani <2201020038@student.umrah.ac.id>
2025-06-09 23:33:45 +0700

    beritaterkini.php

--- beritaterkini.php
+++ beritaterkini.php
@@ -27,46 +27,16 @@ if (isset($_SESSION['success_message'])) {
   unset($_SESSION['success_message']);
 }
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="style/vars.css">
-  <link rel="stylesheet" href="style/beritaterkini.css">
-
-  <style>
-    a,
-    button,
-    input,
-    select,
-    h1,
-    h2,
-    h3,
-    h4,
-    h5,
-    * {
-      box-sizing: border-box;
-      margin: 0;
-      padding: 0;
-      border: none;
-      text-decoration: none;
-      background: none;
-
-      -webkit-font-smoothing: antialiased;
-    }
-
-    menu,
-    ol,
-    ul {
-      list-style-type: none;
-      margin: 0;
-      padding: 0;
-    }
-  </style>
-  <title>berita terkini</title>
+  <meta charset="UTF-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <link rel="stylesheet" href="style/beritaterkini.css" />
+  <title>Berita Terkini</title>
 </head>
 
 <body>
@@ -82,63 +52,66 @@ if (isset($_SESSION['success_message'])) {
         <img class="logoakun" src="svg/profil.svg" alt="Logoakun" onclick="window.location.href='userakun.php'" />
       </div>
     </div>
+
     <div class="search-container">
-      <input type="text" id="searchBox" class="rectangle-1">
-      <img class="search" src="svg/search0.svg" id="searchIcon" />
-      <div class="telusuri" id="placeholderText">Telusuri...</div>
+      <input type="text" id="searchBox" class="rectangle-1" placeholder="Telusuri..." />
+      <img class="search" src="svg/search0.svg" />
     </div>
 
-    <div class="container">
-      <!-- Berita Utama -->
-      <div class="utama">
-        <img src="img/berita1.png" alt="Berita Utama">
-        <div class="text">
-          <h3>Jaksa Agung, Ahok, dan Korupsi Pertamina</h3>
-          <p>Dugaan korupsi tata kelola minyak mentah di anak perusahaan PT Pertamina, Subholding dan KKKS 2018–2023.</p>
-          <a href="berita1.php">Selengkapnya...</a>
+    <div class="slider-container">
+      <div class="slider-wrapper" id="sliderWrapper">
+        <!-- Semua berita jadi card -->
+
+        <div class="card">
+          <img src="img/berita1.png" alt="Berita 1" />
+          <div class="text">
+            <h4>Jaksa Agung, Ahok, dan Korupsi Pertamina</h4>
+            <p>Dugaan korupsi tata kelola minyak mentah di anak perusahaan PT Pertamina, Subholding dan KKKS 2018–2023.</p>
+            <a href="berita1.php">Selengkapnya...</a>
+          </div>
         </div>
+
+        <div class="card">
+          <img src="img/berita2.png" alt="Berita 2" />
+          <div class="text">
+            <h4>Tom Lembong soal Salah Satu Hakimnya Terjerat Kasus Suap CPO</h4>
+            <p>Ketua Satgas Tim Reformasi Hukum, Tom Lembong, menanggapi penahanan salah satu hakim terkait kasus suap minyak goreng (CPO)...</p>
+            <a href="#">Selengkapnya...</a>
+          </div>
+        </div>
+
+        <div class="card">
+          <img src="img/berita3.png" alt="Berita 3" />
+          <div class="text">
+            <h4>Negara Kaya Minyak Jatuh dalam Krisis, Presiden Sebut Darurat Ekonomi</h4>
+            <p>Presiden Kazakstan menyatakan negaranya dalam darurat ekonomi meski kaya minyak, akibat inflasi dan krisis sosial.</p>
+            <a href="#">Selengkapnya...</a>
+          </div>
+        </div>
+
+        <div class="card">
+          <img src="img/berita4.png" alt="Berita 4" />
+          <div class="text">
+            <h4>Kuasa Hukum Pastikan Ijazah Jokowi Tak Pernah Hilang</h4>
+            <p>Kuasa hukum Presiden Jokowi, Yusril Ihza Mahendra, menegaskan bahwa ijazah Jokowi asli dan tidak pernah hilang. Ia membantah tudingan ijazah palsu dan menyebutnya sebagai hoaks.</p>
+            <a href="#">Selengkapnya...</a>
+          </div>
+        </div>
+
+        <div class="card">
+          <img src="img/berita5.png" alt="Berita 5" />
+          <div class="text">
+            <h4>PR Timnas Indonesia U-17 Sebelum Arungi Piala Dunia U-17</h4>
+            <p>Timnas U-17 lolos ke Piala Dunia meski kalah 0-6 dari Korea Utara di Piala Asia. Masih banyak kekurangan yang harus dibenahi sebelum tampil di Qatar.</p>
+            <a href="#">Selengkapnya...</a>
+          </div>
+        </div>
+
       </div>
 
-
-      <!-- Card Berita Lainnya -->
-      <div class="card">
-        <div class="text">
-          <h4>Tom Lembong soal Salah Satu Hakimnya Terjerat Kasus Suap CPO</h4>
-          <p>Ketua Satgas Tim Reformasi Hukum, Tom Lembong, menanggapi penahanan salah satu hakim terkait kasus suap minyak goreng (CPO). Ia menyatakan penangkapan tersebut mengejutkan namun menegaskan komitmen reformasi hukum tetap berjalan. Tom juga menekankan pentingnya integritas aparat hukum untuk memulihkan kepercayaan publik.</p>
-          <a href="#">Selengkapnya...</a>
-        </div>
-        <img src="img/berita2.png" alt="">
-      </div>
-
-      <div class="card">
-        <div class="text">
-          <h4>Negara Kaya Minyak Jatuh dalam Krisis, Presiden Sebut Darurat Ekonomi</h4>
-          <p>Presiden Kazakstan menyatakan negaranya dalam darurat ekonomi meski kaya minyak, akibat inflasi dan krisis sosial.</p>
-          <a href="#">Selengkapnya...</a>
-        </div>
-        <img src="img/berita3.png" alt="">
-      </div>
-
-      <div class="card">
-        <div class="text">
-          <h4>Kuasa Hukum Pastikan Ijazah Jokowi Tak Pernah Hilang</h4>
-          <p>Kuasa hukum Presiden Jokowi, Yusril Ihza Mahendra, menegaskan bahwa ijazah Jokowi asli dan tidak pernah hilang. Ia membantah tudingan ijazah palsu dan menyebutnya sebagai hoaks.</p>
-          <a href="#">Selengkapnya...</a>
-        </div>
-        <img src="img/berita4.png" alt="">
-      </div>
-
-      <div class="card">
-        <div class="text">
-          <h4>PR Timnas Indonesia U-17 Sebelum Arungi Piala Dunia U-17</h4>
-          <p>Timnas U-17 lolos ke Piala Dunia meski kalah 0-6 dari Korea Utara di Piala Asia. Masih banyak kekurangan yang harus dibenahi sebelum tampil di Qatar.</p>
-          <a href="#">Selengkapnya...</a>
-        </div>
-        <img src="img/berita5.png" alt="">
-      </div>
-
+      <button class="slider-btn prev-btn">&lt;</button>
+      <button class="slider-btn next-btn">&gt;</button>
     </div>
-
 
     <footer class="footer">
       <div class="footer-top">
@@ -163,14 +136,14 @@ if (isset($_SESSION['success_message'])) {
         </div>
         <div class="footer-section hubungi-kami">
           <h3>Hubungi Kami</h3>
-          <p>PERPUSTAKAAN ONLINE<br>Tanjungpinang<br>Indonesia</p>
+          <p>PERPUSTAKAAN ONLINE<br />Tanjungpinang<br />Indonesia</p>
           <p>Phone : 081977549410</p>
           <p>Email : xxxxxxx@gmail.com</p>
           <div class="social-icons">
-            <img src="svg/icon-twitter0.svg" alt="Twitter">
-            <img src="svg/icon-facebook0.svg" alt="Facebook">
-            <img src="svg/icon-instagram0.svg" alt="Instagram">
-            <img src="svg/icon-youtube0.svg" alt="YouTube">
+            <img src="svg/icon-twitter0.svg" alt="Twitter" />
+            <img src="svg/icon-facebook0.svg" alt="Facebook" />
+            <img src="svg/icon-instagram0.svg" alt="Instagram" />
+            <img src="svg/icon-youtube0.svg" alt="YouTube" />
           </div>
         </div>
       </div>
@@ -179,37 +152,38 @@ if (isset($_SESSION['success_message'])) {
       </div>
     </footer>
 
-    <script>
-      const searchBox = document.getElementById("searchBox");
-      const placeholderText = document.getElementById("placeholderText");
+  </div>
 
-      const utama = document.querySelector(".utama");
-      const utamaTitle = utama.querySelector("h3").innerText.toLowerCase();
+  <script>
+    const sliderWrapper = document.getElementById('sliderWrapper');
+    const prevBtn = document.querySelector('.prev-btn');
+    const nextBtn = document.querySelector('.next-btn');
+    const searchBox = document.getElementById('searchBox');
 
-      const cards = document.querySelectorAll(".card");
+    const scrollAmount = 320; // scroll per klik
 
-      searchBox.addEventListener("input", function() {
-        const search = this.value.toLowerCase();
-        placeholderText.classList.toggle("hidden", search.length > 0);
+    prevBtn.addEventListener('click', () => {
+      sliderWrapper.scrollBy({ left: -scrollAmount, behavior: 'smooth' });
+    });
 
-        // Filter berita utama
-        if (utamaTitle.includes(search) || search === "") {
-          utama.style.display = "";
-        } else {
-          utama.style.display = "none";
-        }
+    nextBtn.addEventListener('click', () => {
+      sliderWrapper.scrollBy({ left: scrollAmount, behavior: 'smooth' });
+    });
 
-        // Filter card berita lainnya
-        cards.forEach(card => {
-          const title = card.querySelector("h4").innerText.toLowerCase();
-          const paragraph = card.querySelector("p").innerText.toLowerCase();
-          const combinedText = title + " " + paragraph;
+    searchBox.addEventListener('input', function () {
+      const search = this.value.toLowerCase();
 
-          card.style.display = combinedText.includes(search) || search === "" ? "" : "none";
-        });
+      const cards = sliderWrapper.querySelectorAll('.card');
+
+      cards.forEach(card => {
+        const title = card.querySelector('h4').innerText.toLowerCase();
+        const paragraph = card.querySelector('p').innerText.toLowerCase();
+        const combinedText = title + ' ' + paragraph;
+
+        card.style.display = combinedText.includes(search) || search === '' ? '' : 'none';
       });
-    </script>
-
+    });
+  </script>
 </body>
 
 </html>
